8e2f19ba795871a9ecea42d5d398009f43c446b9
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-30 22:20:31 +0300

    ..

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/", "/auth(.*)","/portal(.*)"]
+    publicRoutes: ["/", "/auth(.*)","/portal(.*)"],
+    ignoredRoutes: ['/chatbot']
 })
+
+export const config = {
+    matcher: ['/((?!.+.[w]+$|_next).*)','/','/(api|trpc)(.*)'],
+}
