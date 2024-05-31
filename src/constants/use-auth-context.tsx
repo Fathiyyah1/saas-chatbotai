7506a5cec345e5a3f136c371a875eadba0fcacc1
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-01 00:20:24 +0300

    !!

--- src/constants/use-auth-context.tsx
+++ src/constants/use-auth-context.tsx
@@ -9,3 +9,21 @@ const InilialValues: InitialValuesProps = {
     currentStep:1,
     setCurrentStep: () => undefined,
 }
+
+const authContext = React.createContext(InilialValues)
+const {Provider} = authContext
+
+export const authContextProvider = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    const [currentStep, setCurrentStep] = useState<number>(
+        InilialValues.currentStep
+    )
+     const values = {
+        currentStep, 
+        setCurrentStep,
+        }
+    return <Provider value={values}></Provider>
+}
