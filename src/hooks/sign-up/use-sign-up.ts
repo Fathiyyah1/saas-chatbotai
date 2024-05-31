8c08f55483e6e70cba87feedec5074887b356314
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-01 01:25:11 +0300

    .

--- src/hooks/sign-up/use-sign-up.ts
+++ src/hooks/sign-up/use-sign-up.ts
@@ -1,7 +1,13 @@
 import { useToast } from "@/components/ui/use-toast";
+import { useSignUp } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export const useSignUpForm = () => {
     const {toast} = useToast()
-    const [loading, setLoading] = useState()
+    const [loading, setLoading] = useState<boolean>(false)
+    const {signUp, isLoaded, setActive} = useSignUp()
+    const router = useRouter()
+    const methods = useForm()
 }
