2de5a0e31d39c20bd15b9b3095cb9dd56f06dd9b
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-31 22:55:15 +0300

    ..

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,6 +2,7 @@
 import { currentUser } from "@clerk/nextjs";
 import {redirect} from 'next/navigation';
 import React from "react";
+import Image from "next/image";
 
 type Props = {
     children: React.ReactNode
@@ -11,7 +12,25 @@ const Layout = async ({ children }: Props) => {
     const user = await currentUser()
 
     if(user) redirect('/')
-        return <div>Layout</div>
+        return (
+            
+            <div className="h-screen flex w-full justify-center">
+                <div className="w-[600px] ld:w-full flex flex-col items-start p-6">
+                    <Image 
+                    src="/images/logo.png"
+                    alt="LOGO"
+                    sizes="100vw"
+                    style={{
+                        width: '20%',
+                        height: 'auto',
+                    }}
+                    width={0}
+                    height={0}
+                    />
+                    {children}
+                </div>
+            </div>
+    )
 }
 
 export default Layout
