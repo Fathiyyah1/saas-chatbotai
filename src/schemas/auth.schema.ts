14cdaa6d711966c3bca930a80bb0c07d45692b10
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-01 01:27:51 +0300

    .

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,9 @@
+export type UserRegisterationProps = {
+    type: string 
+    fullname: string 
+    email: string 
+    confirmEmail: string 
+    password: string 
+    confirmPassword: string 
+    otp: string 
+}
